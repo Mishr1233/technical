7f3802d89e5d849f33b2edd4d20aae4d14814c94
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-01-07 16:05:20 +0530

    multiplication function

---
+++ Message.java
@@ -0,0 +1,12 @@
+public class Message{
+public sttaic void main(String args[]){
+
+System.out.println("welcome coder");
+}
+
+
+
+public static void mul(int a,int b){
+System.out.println(a*b);
+}
+}
