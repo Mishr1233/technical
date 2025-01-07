2ae25545ff8efd6d5460a092f46b0df9bb34001e
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-01-07 16:17:30 +0530

    update message

--- Message.java
+++ Message.java
@@ -1,12 +1,8 @@
 public class Message{
 public static void main(String args[]){
 
-System.out.println("welcome coder");
+System.out.println("Hii coder");
 }
 
-
-
-public static void mul(int a,int b){
-System.out.println(a*b);
 }
 }
